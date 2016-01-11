2c16d78956f6b072007ea305c411117e2f4e88f8
Keith Cirkel <github@keithcirkel.co.uk>
2016-01-11 10:11:30 +0000

    nvm: add nvm

---
+++ nvm/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+if [ ! -f "$HOME/.nvm/nvm.sh" ]
+then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | bash
+fi
+source ~/.nvm/nvm.sh
+
+nvm --version
+
+nvm install 0
+nvm install 4
+nvm install 5
+
+nvm alias lts 4
+nvm alias stable 5
+nvm alias default stable
+nvm use default
