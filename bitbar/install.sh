feadbcea09be1a824a38ce9a75276937fdce1bbd
Keith Cirkel <github@keithcirkel.co.uk>
2016-08-15 19:20:41 +0100

    fix(bitbar): don't crash out when there is nothing to stash/apply

--- bitbar/install.sh
+++ bitbar/install.sh
@@ -16,16 +16,16 @@ cd "$THISDIR/plugins" || exit 1
 git remote add upstream https://github.com/matryer/bitbar-plugins.git
 git fetch --all --prune --quiet
 git stash save 'config'
+STASHED=$?
 git checkout upstream/master --force --quiet
-git stash apply 'stash^{/config}'
+[ -z $STASHED ] && git stash apply 'stash^{/config}'
 
 defaults write com.matryer.BitBar NSNavLastRootDirectory -string "$THISDIR/plugins/enabled"
 defaults write com.matryer.BitBar pluginsDirectory -string "$THISDIR/plugins/enabled"
 
 echo "Enabling bitbar plugins"
-rm "$THISDIR/plugins/Enabled/"*
-enablePlugin Time/pomodoro.1s.sh
-enablePlugin Dev/Homebrew/brew-updates.1h.sh
+rm "$THISDIR/plugins/Enabled/"* 2&> /dev/null
+enablePlugin Dev/package_manager.7h.sh
 enablePlugin Dev/GitHub/notifications.30s.py
 enablePlugin Dev/Travis/travis-check.2m.py
 chmod +x "$THISDIR/plugins/Enabled/"*
