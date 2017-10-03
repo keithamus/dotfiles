a3bbea4d55d166b1a9fcbbd601a9ea661fbf1b30
Keith Cirkel <keithamus@github.com>
2017-10-03 23:00:57 +0100

    chore(setup): shoot first, ask questions later

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env sh
 SCRIPT_ROOT="$(dirname "$0")"
 installScript() {
-  printf "Install %s? (Y/n) " "$1"
-  read -r REPLY
-  echo
-  if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-  then
-    echo "###"
-    echo "# Installing $1"
-    echo "###"
-    sh "$SCRIPT_ROOT/$2/install.sh"
-  fi
+  echo "###"
+  echo "# Installing $1"
+  echo "###"
+  sh "$SCRIPT_ROOT/$2/install.sh"
 }
 
 if [ "$(uname -s)" = "Darwin" ]
