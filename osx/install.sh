d0fa5c865a8846e565cc8d58584f95961ad7c61e
Keith Cirkel <keithamus@github.com>
2017-10-07 20:03:26 -0700

    feat(osx): shoot first ask questions later

--- osx/install.sh
+++ osx/install.sh
@@ -1,116 +1,94 @@
 #!/usr/bin/env sh
-printf "Enable menu tray icons? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults read com.apple.systemuiserver menuExtras -array \
-    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-    "/System/Library/CoreServices/Menu Extras/Volume.menu" \
-    "/System/Library/CoreServices/Menu Extras/Clock.menu" \
-    "/System/Library/CoreServices/Menu Extras/Battery.menu"
-fi
+echo "Enabling menu tray icon defaults"
+defaults write com.apple.systemuiserver menuextras -array \
+  "/system/library/coreservices/menu extras/bluetooth.menu" \
+  "/system/library/coreservices/menu extras/airport.menu" \
+  "/system/library/coreservices/menu extras/volume.menu" \
+  "/system/library/coreservices/menu extras/battery.menu"
 
-printf "Tweak Finder defaults? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults write com.apple.finder QLEnableTextSelection -bool true
-  defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
-  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-  defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-  defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-  defaults write com.apple.finder ShowStatusBar -bool true
-  chflags nohidden ~/Library
-  killall Finder
-fi
+echo "Enabling text selection in quicklook"
+defaults write com.apple.finder qlenabletextselection -bool true
 
-printf "Setup 'minimal dark' mode? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults write NSGlobalDomain AppleEnableMenuBarTransparency -int 0
-  defaults write NSGlobalDomain AppleInterfaceStyle Dark
-  defaults write NSGlobalDomain AppleAquaColorVariant -int 6
-  defaults write NSGlobalDomain AppleHighlightColor -string "0.780400 0.815700 0.858800"
-  defaults write com.apple.dock no-glass -bool true
-  defaults write com.apple.dock autohide-delay -float 0.1
-  defaults write com.apple.dock autohide -int 1
-  defaults read com.apple.dock tilesize -int 68
-  defaults write com.apple.dock size-immutable -bool YES
-  defaults write com.apple.dock mineffect scale
-  killall Dock
-fi
+echo "Setting column view as Finder default"
+defaults write com.apple.finder fxpreferredviewstyle -string "clmv"
 
-printf "Speed up animations? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults write -g NSWindowResizeTime -float 0.001
-  defaults write com.apple.dock autohide-time-modifier -float 0.25
-  killall Dock
-  killall Finder
-fi
+echo "Disabling extension change warning in finger"
+defaults write com.apple.finder fxenableextensionchangewarning -bool false
 
-printf "Disable autocorrect? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-fi
+echo "Setting Finder to show full path in title"
+defaults write com.apple.finder _fxshowposixpathintitle -bool true
 
-printf "Set up Screenshots directory? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  mkdir -p ~/Screen\ Shots
-  defaults write com.apple.screencapture location ~/Screen\ Shots
-fi
+echo "Showing all Finder extensions"
+defaults write nsglobaldomain appleshowallextensions -bool true
 
-printf "Set up Safari developer tools? (Y/n) "
-read -r REPLY
-echo
-if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ] || [ "$REPLY" = "" ]
-then
-  defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-  defaults write com.apple.Safari IncludeDevelopMenu -bool true
-  defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-  defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-  defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-  defaults write com.apple.appstore ShowDebugMenu -bool true
-fi
+echo "Showing Finder statusbar"
+defaults write com.apple.finder showstatusbar -bool true
+
+echo "Showing ~/Library"
+chflags nohidden ~/library
+
+echo "Setting inteface style to dark granite"
+defaults write nsglobaldomain appleenablemenubartransparency -int 0
+defaults write nsglobaldomain appleinterfacestyle dark
+defaults write nsglobaldomain appleaquacolorvariant -int 6
+defaults write nsglobaldomain applehighlightcolor -string "0.780400 0.815700 0.858800"
+
+echo "Setting dock to minimal and hidden"
+defaults write com.apple.dock no-glass -bool true
+defaults write com.apple.dock autohide-delay -float 0.1
+defaults write com.apple.dock autohide -int 1
+defaults write com.apple.dock tilesize -int 68
+defaults write com.apple.dock size-immutable -bool yes
+defaults write com.apple.dock mineffect scale
+
+echo "Speeding up animations"
+defaults write -g nswindowresizetime -float 0.001
+defaults write com.apple.dock autohide-time-modifier -float 0.25
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g QLPanelAnimationDuration -float 0.25
+defaults write com.apple.finder DisableAllAnimations -bool true
+defaults write com.apple.dock expose-animation-duration -float 0.25
+
+echo "Speeding up keyboard repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+
+echo "Setting save panel to always be in expaneded mode"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -int 1;
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -int 1
+
+killall Dock
+killall Finder
+
+echo "Disabling autocorrect, use Alfred"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+
+echo "Setting up Screenshots directory"
+mkdir -p ~/Screen\ Shots
+defaults write com.apple.screencapture location ~/Screen\ Shots
+
+echo "Setting up Safari developer tools"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write com.apple.appstore ShowDebugMenu -bool true
 
 if ! [ -L ~/Documents ]
 then
-  printf "Do you want to link ~/Documents and ~/Dropbox? (y/n) "
-  read -r REPLY
-  echo
-  if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ]
-  then
-    echo "› Linking ~/Documents and ~/Dropbox..."
-    mv ~/Documents/* ~/Dropbox
-    sudo rm -rf ~/Documents
-    ln -s ~/Dropbox ~/Documents
-  fi
+  echo "Linking ~/Documents and ~/Dropbox..."
+  mv ~/Documents/* ~/Dropbox
+  sudo rm -rf ~/Documents
+  ln -s ~/Dropbox ~/Documents
 fi
 
 if ! [ -L ~/Desktop ]
 then
-  printf "Do you want to nuke the desktop and link to ~/Downloads? (y/n) "
-  read -r REPLY
-  echo
-  if [ "$REPLY" = "Y" ] || [ "$REPLY" = "y" ]
-  then
-    echo "› Linking ~/Desktop and ~/Downloads"
-    mv ~/Desktop/* ~/Downloads
-    sudo rm -rf ~/Desktop
-    ln -s ~/Downloads ~/Desktop
-    defaults write com.apple.finder CreateDesktop false;
-  fi
+  echo "Linking ~/Desktop and ~/Downloads"
+  mv ~/Desktop/* ~/Downloads
+  sudo rm -rf ~/Desktop
+  ln -s ~/Downloads ~/Desktop
+  defaults write com.apple.finder CreateDesktop false;
 fi
