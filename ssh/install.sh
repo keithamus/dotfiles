d0317abfa19581acce6fe44e89f5e51d4940326b
Keith Cirkel <keithamus@users.noreply.github.com>
2020-06-23 21:28:07 +0100

    fix(ssh): fix ssh-keygen issues

--- ssh/install.sh
+++ ssh/install.sh
@@ -11,22 +11,21 @@ fi
 GHUSER="${STRAP_GITHUB_USER:-keithamus}"
 
 pub="$(ssh-keygen -y -f ~/.ssh/id_rsa | cut -d ' ' -f 1,2)"
-pubfile="~/.ssh/id_rsa.pub"
 url="https://github.com/settings/ssh/new"
 if ! curl -sq "https://github.com/$GHUSER.keys" | grep "$pub" > /dev/null; then
   echo "Add your SSH key to GitHub.com!"
-  if [ "$(uname -s)" = "darwin" ]
+  if [ "$(uname -s)" = "Darwin" ]
   then
-    pbcopy < "$pubfile"
+    echo "$pub" | pbcopy
     echo "The public key is in your clipboard"
     open "$url"
   elif [ "$(uname -s)" = "Linux" -a "$GDMSESSION" = "pop" ]
   then
-    xclip -selection clipboard < "$pubfile"
+    echo "$pub" | xclip -selection clipboard
     echo "The public key is in your clipboard"
     gio open "$url"
   else
-    cat "$pubfile"
+    echo "$pub"
     echo "Copy the contents of this public key, and visit $url"
   fi
 fi
