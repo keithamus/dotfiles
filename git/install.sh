df60b4f2d28ea97212e158a8dc96526b3e2d8d0d
Keith Cirkel <keithamus@users.noreply.github.com>
2020-07-22 11:27:21 +0100

    feat(git): add git-codeowners

--- git/install.sh
+++ git/install.sh
@@ -9,9 +9,16 @@ then
 elif [ "$(uname -s)" = "Linux" -a "$GDMSESSION" = "pop" ]
 then
   sudo apt install -qq -y --no-install-recommends git tig
+  
+  # Git-Delta
   wget https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb
   sudo dpkg --skip-same-version -i git-delta_0.1.1_amd64.deb
   rm -f git-delta_0.1.1_amd64.deb
+
+  # Git-Codeowners
+  curl -L https://github.com/softprops/git-codeowners/releases/download/v0.1.2/git-codeowners-Linux-x86_64.tar.gz | tar -xz
+  mv ./git-codeowners /usr/local/bin
+
 fi
 
 GITEDITOR="$(git config --global core.editor)"
