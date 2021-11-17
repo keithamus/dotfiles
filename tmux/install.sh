ef238c81011f1d4d8b8b9bb914503660e9169aeb
Keith Cirkel <keithamus@users.noreply.github.com>
2021-11-17 11:34:20 +0000

    feat(tmux): tweaks

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
-"$(dirname "$0")/../auto-install.sh" "$(basename $(dirname $0))"
+"$(dirname "$0")/../auto-install.sh" $0
 
 THISDIR=$(cd "$(dirname "$0")"; pwd)
 ln -sf "${THISDIR}/tmux.conf"  ~/.tmux.conf
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
