0bc466e4ed586ed461964cd4a8aa60ea53eefd4a
Keith Cirkel <keithamus@users.noreply.github.com>
2021-11-17 11:36:53 +0000

    feat(fish): improve gpg-agent integration

--- fish/conf.d/gpu-agent.fish
+++ fish/conf.d/gpu-agent.fish
@@ -1,1 +1,8 @@
-gpg-agent --daemon > /dev/null ^ /dev/null
+gpg-agent -q
+if test $status -ne "0"
+  gpg-agent --daemon > /dev/null ^ /dev/null
+end
+set -xg GPG_TTY (tty)
+set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpg-connect-agent updatestartuptty /bye > /dev/null
+gpgconf --launch gpg-agent
