4d9cfd0899945d81f8fcbaad839e47e43878b4c3
Keith Cirkel <keithamus@users.noreply.github.com>
2021-11-23 14:46:20 +0000

    fix(git): shellcheck install script

--- git/install.sh
+++ git/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
-"$(dirname "$0")/../auto-install.sh" $0
+"$(dirname "$0")/../auto-install.sh" "$0"
 
-if [ "$(which apt 2>/dev/null)"]; then
+if [ "$(which apt 2>/dev/null)" ]; then
   # Git-Delta
-  wget https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb
+  wget -q https://github.com/dandavison/delta/releases/download/0.1.1/git-delta_0.1.1_amd64.deb
   sudo dpkg --skip-same-version -i git-delta_0.1.1_amd64.deb
   rm -f git-delta_0.1.1_amd64.deb
 fi
@@ -11,10 +11,8 @@ fi
 GITEDITOR="$(git config --global core.editor)"
 GITNAME="$(git config --global user.name)"
 GITEMAIL="$(git config --global user.email)"
-THISDIR=$(cd "$(dirname "$0")"; pwd)
+THISDIR=$(cd "$(dirname "$0")" || exit 1; pwd)
 GITCONFIG="$THISDIR/gitconfig"
-TIGCONFIG="$THISDIR/tigrc"
-
 
 mkdir -p ~/.config/git
 git config --global core.editor "${GITEDITOR:-nvim}"
@@ -22,9 +20,9 @@ git config --global user.name "${GITNAME:-Keith Cirkel}"
 git config --global user.email "${GITEMAIL:-keithamus@users.noreply.github.com}"
 git config --global include.path "$GITCONFIG"
 
-GPG_ID=$(gpg --list-secret-keys --keyid-format LONG $GITEMAIL | head -1 | awk '{print $2}' | awk -F '/' '{print $2}')
+GPG_ID=$(gpg --list-secret-keys --keyid-format LONG "$GITEMAIL" 2>/dev/null | head -1 | awk '{print $2}' | awk -F '/' '{print $2}')
 if [ "$GPG_ID" ]; then
-  git config --global user.signingkey $GPG_ID
+  git config --global user.signingkey "$GPG_ID"
 fi
 
 USERTIGCONFIG="$HOME/.tigrc"
