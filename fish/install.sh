296d60a835fef84b0f99b77e06077acb2b3cd7bf
Keith Cirkel <keithamus@users.noreply.github.com>
2021-11-24 16:53:07 +0000

    fix(fish): clean up fish installer

--- fish/install.sh
+++ fish/install.sh
@@ -6,18 +6,22 @@ mkdir -p ~/.config/fish/conf.d
 ln -sf "${THISDIR}/fishfile"  ~/.config/fish/fishfile
 ln -sf "${THISDIR}/conf.d/"* ~/.config/fish/conf.d/
 ln -sf "${THISDIR}/functions/"* ~/.config/fish/functions/
+FISH="$(which fish 2>/dev/null)"
 
 if [ "$(grep -c fish /etc/shells)" = "0" ]
 then
   echo "Please enter your password to add fish to available shells"
-  which fish 2>/dev/null | sudo tee -a /etc/shells
+  echo $FISH | sudo tee -a /etc/shells
 fi
-chsh -s "$(which fish 2>/dev/null)"
 
-echo "Installing fisherman"
+if [ "$SHELL" != "$FISH" ]; then
+  echo "Changing shell"
+  chsh -s "$(which fish 2>/dev/null)"
+fi
 
-if ! fish -c 'fisher --version' 2> /dev/null
-then
+if ! fish -c 'fisher --version' 2> /dev/null; then
+  echo "Installing fisherman"
   curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
 fi
-fish -c 'fisher'
+
+fish -c 'fisher update'
