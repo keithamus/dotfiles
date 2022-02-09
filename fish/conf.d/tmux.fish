3676e45ece75907dbe3fd792ced9dd6f922deba5
Keith Cirkel <keithamus@users.noreply.github.com>
2022-02-09 09:31:58 +0000

    remove tmux

--- fish/conf.d/tmux.fish
+++
@@ -1,3 +0,0 @@
-if test $TERM != "screen"; and test "$TMUX" = ""; and test -t 0
-  tmux -u attach || tmux -u
-end
--- tmux/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-"$(dirname "$0")/../auto-install.sh" "$0"
-
-THISDIR=$(cd "$(dirname "$0")" || exit 1; pwd)
-ln -sf "${THISDIR}/tmux.conf"  ~/.tmux.conf
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
