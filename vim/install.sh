398a1c24b0381c6b019fefa9055efcaca103ecca
Keith Cirkel <keithamus@users.noreply.github.com>
2022-04-01 11:01:55 +0100

    add nvim latest to codespace

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 "$(dirname "$0")/../auto-install.sh" "$0"
+OS=$("$(dirname "$0")/../os.sh")
+
+if [ "$OS" = "codespace" ]; then
+  sudo apt install -y fuse libfuse2
+  sudo groupadd fuse
+  sudo usermod -aG fuse "$(whoami)"
+  wget https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+  chmod u+x nvim.appimage
+  sudo mv nvim.appimage /usr/local/bin/nvim
+fi
 
 
 echo "Install/update packer.nvim"
