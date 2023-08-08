1b706a831214a8f48ed07afa5110aa50f3a2b045
Keith Cirkel <keithamus@users.noreply.github.com>
2023-08-08 21:34:54 +0100

    fish updates

--- fish/conf.d/abbrs.fish
+++ fish/conf.d/abbrs.fish
@@ -3,21 +3,20 @@ abbr g 'git'
 abbr gs 'git st'
 abbr ga 'git add -p'
 abbr gpf 'git push --force-with-lease'
-abbr gpu 'git push --set-upstream origin (git rev-parse --abbrev-ref HEAD) | grep http | sed \'s/remote: *//\' | xargs open'
+abbr gpu 'git push --set-upstream origin (git rev-parse --abbrev-ref HEAD); and hub compare;'
 abbr gpr 'hub pull-request -o'
 abbr gcm 'git commit --message'
 abbr gcw 'git commit --message \'WIP!\''
 abbr gc 'git commit'
 abbr gcan 'git commit --amend --no-edit'
 abbr gfcm 'git fetch --all --prune; and git branch --merged origin/master | tail -n +2 | xargs git branch -d; and git checkout origin/master'
-abbr grim 'git fetch; and git rebase -i origin/master'
+abbr griom 'git fetch; and git rebase -i origin/master'
 abbr gcl 'git clone git@github.com:'
 abbr gba 'git checkout -b (git log --format=%s | head -1 | tr -cs \[:alnum:\] "-" | tr \[:upper:\] \[:lower:\] | sed \'s/-$//\')'
 
 # nvim Abbreviations!
 abbr nf 'nvim (fzf)'
 abbr ng 'nvim (git status --porcelain | grep "^UU" | head -1 | cut -f2 -d" ")'
-
 
 # npm Abbrevations!
 abbr npt 'npm test'
@@ -27,5 +26,5 @@ abbr npb 'npm run build --'
 abbr npl 'npm run lint --'
 abbr npit 'npm it'
 
-# taskwarrior abbreviations
-abbr t 'task project:(basename (pwd))'
+# journal
+abbr jim 'nvim ~/Journal/(date +%Y)/(date +%Y-%m-%d).md +\'!mkdir -p %:h\' +w +i'
