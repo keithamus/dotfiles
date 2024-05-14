11e3cd58193205875c436b203858e1372cbf3a73
Keith Cirkel <keithamus@users.noreply.github.com>
2024-05-14 15:30:28 +0100

    vim updates

--- vim/config/nvim/lua/config/options.lua
+++ vim/config/nvim/lua/config/options.lua
@@ -29,7 +29,16 @@ opt.expandtab = true
 
 -- disable annoying folding and concealing
 opt.conceallevel = 0
-opt.foldenable = false
+
+-- unabashedly stolen from https://github.com/jackfranklin/dotfiles/blob/master/nvim/lua/jack/folds.lua
+vim.opt.foldcolumn = "0"
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = ""
+
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 4
 
 -- enable backups
 opt.backup = true
