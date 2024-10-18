28a9748f630c3a8ad422b0b1df87b9557aa54c18
Keith Cirkel <keithamus@users.noreply.github.com>
2024-10-18 09:13:46 +0100

    fish - fix up gpg agent to 1p

--- fish/conf.d/gpu-agent.fish
+++ fish/conf.d/gpu-agent.fish
@@ -1,8 +1,8 @@
 gpg-agent -q
-if test $status -ne "0"
-  gpg-agent --daemon > /dev/null ^ /dev/null
+if test $status -ne 0
+    gpg-agent --daemon >/dev/null ^ /dev/null
 end
 set -xg GPG_TTY (tty)
-set -xg SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-gpg-connect-agent updatestartuptty /bye > /dev/null
+set -xg SSH_AUTH_SOCK ~/.1password/agent.sock
+gpg-connect-agent updatestartuptty /bye >/dev/null
 gpgconf --launch gpg-agent
