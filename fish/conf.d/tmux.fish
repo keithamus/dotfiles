799d020e1c2cadcea2ed782ae97d1858fb66ffe9
Keith Cirkel <keithamus@users.noreply.github.com>
2024-12-03 20:26:13 +0000

    prevent tmux in vscode terminal

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,3 +1,3 @@
-if test $TERM != "screen"; and test "$TMUX" = ""; and test -t 0
+if test $TERM != "screen"; and test "$TMUX" = ""; and test -t 0; and test "$TERM_PROGRAM" != "vscode"
   tmux -u attach || tmux -u
 end
